d61058101849df284ba5274a0409263d64837a5b
Anton Bogomazov <anton.p.bogomazov@gmail.com>
2022-05-04 12:58:04 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const availableLanguages = ['en', 'ru'];
+const defaultLanguage = 'en';
+
+let currentLanguage = 'en';
+
+const whenReady = (callback) => {
+  if (document.readyState !== "loading") {
+    callback();
+  } else {
+    document.addEventListener("DOMContentLoaded", callback);
+  }
+}
+
+function updateLanguageButton(lang) {
+  const activeClassName = 'business-card__language-button--active';
+
+  document.querySelector(`[data-language=${currentLanguage}]`).classList.remove(activeClassName);
+  document.querySelector(`[data-language=${lang}]`).classList.add(activeClassName);
+}
+
+function switchLanguage(lang) {
+  const targetLang = lang.toLowerCase();
+  const businessCard = document.querySelector('.business-card');
+
+  businessCard.classList.remove(`business-card--${currentLanguage}`);
+  businessCard.classList.add(`business-card--${targetLang}`);
+  window.history.replaceState(null, null, `?lang=${targetLang}`);
+
+  switch (targetLang) {
+    case 'ru': document.title = 'Антон Богомазов - Java/Kotlin Разработчик'; break;
+    case 'en': document.title = 'Anton Bogomazov - Java/Kotlin Developer'; break;
+  }
+  updateLanguageButton(targetLang)
+
+  currentLanguage = targetLang;
+}
+
+function parseLangFromUri() {
+  const lang = document.location.href.split('?lang=')[1];
+  if (lang == null || !availableLanguages.includes(lang.toLowerCase())) return defaultLanguage;
+  return lang.toLowerCase();
+}
+
+function updateLanguageFromUri() {
+  const lang = parseLangFromUri();
+  switchLanguage(lang);
+  currentLanguage = lang;
+}
+
+function addLangSwitchListeners() {
+  availableLanguages.forEach( (lang) => {
+      const button = document.querySelector(`[data-language=${lang}]`)
+      if (button != null) {
+        button.addEventListener('click', () => switchLanguage(lang));
+      }
+  })
+}
+
+whenReady(() => {
+  updateLanguageFromUri();
+  addLangSwitchListeners();
+});
